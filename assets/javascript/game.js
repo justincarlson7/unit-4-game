bc51c0c0a9de3a12dc71b14a84d9215296f0ee37
Justin Carlson <justincarlson7@gmail.com>
2019-08-30 12:00:43 -0400

    working on logic to save random number variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,78 +8,85 @@
 
 //  console.log("This is the computer number " + compRandoNumber);
 
+//Create random number for the computer
+
 $(document).ready(function() {
   var compRandoNumber = 0;
 
   var jewelOne = 0;
   var jewelTwo = 0;
   var jewelThree = 0;
-  var jewelOneTotal =0;
+  var jewelOneTotal = 0;
+  var jewelCounter = 0;
+  var test = 5;
+
+  function randomNum1() {
+    ranNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    return ranNum;
+  }
+
+jewelOne = randomNum1();
+
+jewelTwo = randomNum1();
+
+jewelThree = randomNum1();
+
 
   function ranComp() {
     compRandoNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 1;
-  }
 
- 
+    return compRandoNumber;
+  }
 
   ranComp();
 
   console.log("This is the computer number " + compRandoNumber);
 
-
-
+  console.log("This is test: " + test);
 
   $("#jewel1").on("click", function() {
-    function ran1() {
-      randomNum1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    }
+    // console.log("This is my random number: " + randomNum1);
 
-    ran1();
+    // function jewelOneAdder(randomNum1, jewelOne) {
 
-    console.log("This is my random number: " + randomNum1);
-      
-    function jewelOneAdder(randomNum1, jewelOne) {
+    // }
+    // jewelOneTotal += jewelOne;
+    jewelCounter += jewelOne;
 
-       
-        var jewelOneTotal = randomNum1 + jewelOne;
-           
-       
+    console.log("This is my counter  total: " + jewelCounter);
+
   
-    jewelOneAdder();
+    // console.log("This is my jewel one total: " + jewelOneTotal);
 
-    console.log("This is my jewel one total: " + jewelOneTotal);
+    $("#addHere1").append("<div class='col-md-12'>" + jewelOne + "</div>");
+  });
 
 
-    $("#addHere1").append("<div class='col-md-12'>" + randomNum1 + "</div>");
-  }
-
-});
 
   $("#jewel2").on("click", function() {
-    function ran2() {
-      randomNum2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    }
+  
+    console.log("This is my random number: " + jewelTwo);
 
-    ran2();
+    $("#addHere2").append("<div class='col-md-12'>" + jewelTwo + "</div>");
 
-    console.log("This is my random number: " + randomNum2);
 
-    $("#addHere2").append("<div class='col-md-12'>" + randomNum2 + "</div>");
   });
+
+
+
+
   $("#jewel3").on("click", function() {
-    function ran3() {
-      randomNum3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    }
+   
+  
 
-    ran3();
+    console.log("This is my random number: " + jewelThree);
 
-    console.log("This is my random number: " + randomNum3);
-
-    $("#addHere3").append("<div class='col-md-12'>" + randomNum3 + "</div>");
-
-
-     
+    $("#addHere3").append("<div class='col-md-12'>" + jewelThree + "</div>");
   });
 });
+
+//if 
+
+// function at end of each on click that checks if equal or over - then you win or you lose.
 
 // $("p").append("Some appended text.");
