2417eae81e33d003ab4adf59b45478ad76a730ba
Justin Carlson <justincarlson7@gmail.com>
2019-09-02 20:06:28 -0400

    All and I mean all of the logic is now working for the game. I'm working on the design. I still do not have all the final text treatments yet. But working on it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@
 
 //  ranComp();
 
-//  console.log("This is the computer number " + compRandoNumber);
+//  $("#compRandoNumberHtml").html("<a> Match this number:" + compRandoNumber + "</a>");
 
 //Create random number for the computer
 
@@ -16,35 +16,39 @@ $(document).ready(function() {
   var jewelOne = 0;
   var jewelTwo = 0;
   var jewelThree = 0;
-  var jewelOneTotal = 0;
+  var jewelFour = 0;
   var jewelCounter = 0;
-  var test = 5;
+  var wins = 0;
+  var losses = 0;
 
   function randomNum1() {
     ranNum = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     return ranNum;
   }
 
-jewelOne = randomNum1();
+  jewelOne = randomNum1();
 
-jewelTwo = randomNum1();
+  jewelTwo = randomNum1();
 
-jewelThree = randomNum1();
+  jewelThree = randomNum1();
 
+  jewelFour = randomNum1();
 
   function ranComp() {
-    compRandoNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 1;
-
+    compRandoNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     return compRandoNumber;
   }
+ 
 
   ranComp();
 
-  console.log("This is the computer number " + compRandoNumber);
+  $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
 
-  console.log("This is test: " + test);
+  $("#winsHtml").html("<h2> Wins: 0</h2>");
+  $("#lossesHtml").html("<h2> Losses: 0</h2>");
+  $("#jewelCounterHtml").html("<h2> Number of Bugs: 0</h2>");
 
-  $("#jewel1").on("click", function() {
+  $("#orangeBug").on("click", function() {
     // console.log("This is my random number: " + randomNum1);
 
     // function jewelOneAdder(randomNum1, jewelOne) {
@@ -54,83 +58,184 @@ jewelThree = randomNum1();
     jewelCounter += jewelOne;
 
     if (jewelCounter === compRandoNumber) {
+      wins++;
+      $("#winsHtml").html("<h2> Wins:" + wins + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Win!!!!</h2>");
+      $("#yourOutcomeHtml" ).fadeOut(5000);
+      //need a jquery remove somehwhere
+      console.log("You win!!!" + wins);
 
-console.log("You win!!!");
+      ranComp();
+      jewelOne = randomNum1();
 
-    }
+      jewelTwo = randomNum1();
 
-    else if (jewelCounter >= compRandoNumber) { 
+      jewelThree = randomNum1();
 
+      jewelFour = randomNum1();
+      jewelCounter = 0;
 
-console.log("You lose!!!");
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    } else if (jewelCounter >= compRandoNumber) {
+      losses++;
+      $("#lossesHtml").html("<h2> Loses:" + losses + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Lose!!!!</h2>");
+      $( "#yourOutcomeHtml" ).fadeOut(5000);
+      //need a jquery remove somehwhere
+      console.log("<h2> You Lose!!!!</h2>" + losses);
 
+      ranComp();
+      jewelOne = randomNum1();
 
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
     }
 
     //console.log("This is my counter  total: " + jewelCounter);
 
-  
     // console.log("This is my jewel one total: " + jewelOneTotal);
 
-    $("#jewelCounterHtml").html("<a>" + jewelCounter + "</a>");
+    $("#jewelCounterHtml").html("<h2> Number of Bugs: " + jewelCounter + "</h2>");
   });
 
-
-
-  $("#jewel2").on("click", function() {
-
+  $("#coffeeBug").on("click", function() {
     jewelCounter += jewelTwo;
 
     if (jewelCounter === compRandoNumber) {
+      wins++;
+      $("#winsHtml").html("<h2> Wins:" + wins + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Win!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
+      console.log("You win!!!" + wins);
 
-      console.log("You win!!!");
-      
-          }
-      
-          else if (jewelCounter >= compRandoNumber) { 
-      
-      
-      console.log("You lose!!!");
-      
-      
-          }
-  
-   // console.log("This is my random number: " + jewelTwo);
+      ranComp();
+      jewelOne = randomNum1();
 
-    $("#jewelCounterHtml").html("<a>" + jewelCounter + "</a>");
+      jewelTwo = randomNum1();
 
+      jewelThree = randomNum1();
 
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    } else if (jewelCounter >= compRandoNumber) {
+      losses++;
+      $("#lossesHtml").html("<h2> Losses:" + losses + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Lose!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
+
+      console.log("<h2> You Lose!!!!</h2>" + losses);
+
+      ranComp();
+      jewelOne = randomNum1();
+
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    }
+
+    // console.log("This is my random number: " + jewelTwo);
+
+    $("#jewelCounterHtml").html("<h2> Number of Bugs: " + jewelCounter + "</h2>");
   });
 
-
-
-
-  $("#jewel3").on("click", function() {
-   
-  
+  $("#blueBug").on("click", function() {
     jewelCounter += jewelThree;
+
+    if (jewelCounter === compRandoNumber) {
+      wins++;
+      $("#winsHtml").html("<h2> Wins:" + wins + "</h2>");
+      console.log("You win!!!" + wins);
+      $("#yourOutcomeHtml").html("<h2> You Win!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
+
+      ranComp();
+      jewelOne = randomNum1();
+
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    } else if (jewelCounter >= compRandoNumber) {
+      losses++;
+      $("#lossesHtml").html("<h2> Losses:" + losses + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Lose!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
+      console.log("<h2> You Lose!!!!</h2>" + losses);
+
+      ranComp();
+      jewelOne = randomNum1();
+
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    }
+
+    $("#jewelCounterHtml").html("<h2> Number of Bugs: " + jewelCounter + "</h2>");
+  });
+
+  $("#silverBug").on("click", function() {
+    jewelCounter += jewelFour;
     // console.log("This is my random number: " + jewelThree);
 
-     if (jewelCounter === compRandoNumber) {
+    if (jewelCounter === compRandoNumber) {
+      wins++;
+      $("#winsHtml").html("<h2> Wins:" + wins + "</h2>");
+      console.log("You win!!!" + wins);
+      $("#yourOutcomeHtml").html("<h2> You Win!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
 
-console.log("You win!!!");
+      ranComp();
+      jewelOne = randomNum1();
 
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
+    } else if (jewelCounter >= compRandoNumber) {
+      losses++;
+      $("#lossesHtml").html("<h2> Losses:" + losses + "</h2>");
+      $("#yourOutcomeHtml").html("<h2> You Lose!!!!</h2>");
+      // $("#yourOutcomeHtml" ).fadeOut(5000);
+      console.log("<h2> You Lose!!!!</h2>" + losses);
+
+      ranComp();
+      jewelOne = randomNum1();
+
+      jewelTwo = randomNum1();
+
+      jewelThree = randomNum1();
+
+      jewelFour = randomNum1();
+      jewelCounter = 0;
+      $("#compRandoNumberHtml").html("<h2> Match this number: " + compRandoNumber + "</h2>");
     }
 
-    else if (jewelCounter >= compRandoNumber) { 
-
-
-console.log("You lose!!!");
-
-
-    }
-
-    $("#jewelCounterHtml").html("<a>" + jewelCounter + "</a>");
-
+    $("#jewelCounterHtml").html("<h2> Number of Bugs: " + jewelCounter + "</h2>");
   });
 });
 
-//if 
+//if
 
 // function at end of each on click that checks if equal or over - then you win or you lose.
 
